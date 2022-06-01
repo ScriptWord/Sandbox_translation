8b85efbc5097b79e9146b85f052c87430d2d8944
sashank <mssashankpromo@gmail.com>
2022-06-01 00:54:21 -0400

    basc commit _2

--- components/homepage/HomeBanner.js
+++ components/homepage/HomeBanner.js
@@ -7,9 +7,7 @@ export default function HomeBanner() {
       <p
         className="font-size-display1 mt-5 text-center mx-auto text-uppercase"
         style={{ maxWidth: '53rem' }}
-      >
-        Canada's Best Translation Service.
-      </p>
+      >Canadas Best Translation Service.</p>
       <div className="d-flex align-items-center justify-content-center mt-3 mb-5">
         <Link href="/about">
           <a className="d-flex py-3 align-items-center font-color-black borderbottom border-color-black">
